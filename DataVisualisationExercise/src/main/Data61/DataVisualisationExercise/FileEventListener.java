bef8d3ae6738641a2c31887f9406811849934f59
Mansour Khelghatdoust <mansour475@yahoo.com>
2018-03-16 03:31:23 +1100

    Implement a general class to read datasets from files

---
+++ DataVisualisationExercise/src/main/Data61/DataVisualisationExercise/FileEventListener.java
@@ -0,0 +1,5 @@
+package Data61.DataVisualisationExercise;
+
+public interface FileEventListener {
+	public void onRead(String line);
+}
---
+++ DataVisualisationExercise/src/main/Data61/DataVisualisationExercise/FileReaderIterator.java
@@ -0,0 +1,50 @@
+package Data61.DataVisualisationExercise;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class FileReaderIterator implements Iterator<String> {
+
+	private String fileName;
+	private String line = null;
+	private FileEventListener listener;
+	private FileReader fileReader = null;
+	private BufferedReader bufferedReader = null;
+
+	public FileReaderIterator(String fileName, FileEventListener listener) {
+		this.fileName = fileName;
+		this.listener = listener;
+		read();
+	}
+
+	private void read() {
+		try {
+			fileReader = new FileReader(fileName);
+			bufferedReader = new BufferedReader(fileReader);
+		} catch (FileNotFoundException ex) {
+			System.out.println("Unable to open file '" + fileName + "'");
+		}
+	}
+
+	@Override
+	public boolean hasNext() {
+		try {
+			if (bufferedReader.ready() && (line = bufferedReader.readLine()) != null)
+				return true;
+			bufferedReader.close();
+		} catch (IOException ex) {
+			System.out.println("Error reading file '" + fileName + "'");
+		}
+		return false;
+	}
+
+	@Override
+	public String next() {
+		if (line != null)
+			listener.onRead(line);
+		return line;
+	}
+}
