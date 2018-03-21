7ae12cc5501bef9ddb98a06e8d25be2935d35262
Mansour Khelghatdoust <mansour475@yahoo.com>
2018-03-21 17:06:14 +1100

    add testcases for Authors and Papers

--- DataVisualisationExercise/src/test/Data61/DataVisualisationExercise/GraphVisualisationAppTest.java
+++ DataVisualisationExercise/src/test/Data61/DataVisualisationExercise/GraphVisualisationAppTest.java
@@ -1,38 +1,12 @@
 package Data61.DataVisualisationExercise;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
-/**
- * Unit test for simple App.
- */
-public class GraphVisualisationAppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public GraphVisualisationAppTest( String testName )
-    {
-        super( testName );
-    }
+@RunWith(Suite.class)
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( GraphVisualisationAppTest.class );
-    }
+@Suite.SuiteClasses({ AuthorsTest.class })
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class GraphVisualisationAppTest {
+	
 }
