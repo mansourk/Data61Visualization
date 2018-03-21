255bd7411c53f710b336fdeb00f9682e4ee1671d
Mansour Khelghatdoust <mansour475@yahoo.com>
2018-03-22 03:20:28 +1100

    add test suite class

--- DataVisualisationExercise/src/test/Data61/DataVisualisationExercise/GraphVisualisationAppTest.java
+++ DataVisualisationExercise/src/test/Data61/DataVisualisationExercise/GraphVisualisationAppTest.java
@@ -5,8 +5,9 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 
-@Suite.SuiteClasses({ AuthorsTest.class })
+@Suite.SuiteClasses({ AuthorsTest.class, BoundedPositioningTest.class, CoordinateSystemTest.class,
+		NearestNeighborTest.class, NetworkTraversalTest.class, RotationTest.class })
 
 public class GraphVisualisationAppTest {
-	
+
 }
